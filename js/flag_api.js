843d3f56e3405772e1054b2d861d38c3ba5ad627
de-sipher <0mu5sfm4@anonaddy.me>
2022-05-05 12:59:29 +0530

    wrong icon colour on first time page load. -- bug fixed

--- js/flag_api.js
+++ js/flag_api.js
@@ -24,7 +24,7 @@ fetch(apiUrl).then((data) => {
     countryListjson = data
     console.log(data)
 }).then(() => {
-    for (let i = 1; i <= 6; i++) {
+    for (let i = 1; i <= 10; i++) {
         var randCountry = getRandom(countryListjson.length)
         countryList.appendChild(makeNode(countryListjson[randCountry], randCountry))
     }
